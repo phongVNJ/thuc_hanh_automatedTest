4f19766daefa11b049a2ce07e954e15407a9699d
= <=>
2020-05-19 17:27:27 +0700

    add

---
+++ src/test/java/CustomerServiceTestConfig.java
@@ -0,0 +1,19 @@
+import cg.wbd.grandemonstration.repository.CustomerRepository;
+import cg.wbd.grandemonstration.service.CustomerService;
+import cg.wbd.grandemonstration.service.impl.CustomerServiceImplWithSpringData;
+import org.mockito.Mockito;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CustomerServiceTestConfig {
+    @Bean
+    public CustomerRepository customerRepository() {
+        return Mockito.mock(CustomerRepository.class);
+    }
+
+    @Bean
+    public CustomerService customerService() {
+        return new CustomerServiceImplWithSpringData();
+    }
+}
